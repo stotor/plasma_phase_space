Add p2p1 calculation.  Fix axis indexing in field saving.

triangle_calculations.py
@@ -188,6 +188,47 @@ def deposit_triangles_momentum(cartcomm, pos, mom, charge, grid, window, box, ou
 
     return
 
+def deposit_triangles_p2p1(cartcomm, mom, charge, grid, window, box, output_folder, species, t, time, axis):
+    if t == 0:
+        return
+    
+    rank = cartcomm.Get_rank()
+
+    mom = np.array(mom[:,:,1:], copy=True)
+
+    vel_dummy = np.zeros_like(mom)
+    
+    fields = {'m': None}
+    psi.elementMesh(fields, mom, vel_dummy, charge, grid=grid, window=window, box=box, periodic=False)
+    p2p1 = fields['m']
+    p2p1_total = np.zeros_like(p2p1)
+
+    cartcomm.Reduce([p2p1, MPI.DOUBLE], [p2p1_total, MPI.DOUBLE], op = MPI.SUM, root = 0)
+
+    if (rank==0):        
+        utilities.save_density_field_attrs(output_folder, 'p2p1-ed', species, t, time, p2p1_total, axis)
+
+    return
+
+def deposit_particles_p2p1(cartcomm, mom, charge, grid, window, box, output_folder, species, t, time, axis, deposit):
+    rank = cartcomm.Get_rank()
+
+    mom = np.array(mom[:,1:], copy=True)
+
+    vel_dummy = np.zeros_like(mom)
+    
+    fields = {'m': None}
+    psi.particleMesh(fields, mom, vel_dummy, charge, grid=grid, window=window, box=box, periodic=False, sampling=deposit)
+    p2p1 = fields['m']
+    p2p1_total = np.zeros_like(p2p1)
+
+    cartcomm.Reduce([p2p1, MPI.DOUBLE], [p2p1_total, MPI.DOUBLE], op = MPI.SUM, root = 0)
+
+    if (rank==0):        
+        utilities.save_density_field_attrs(output_folder, 'p2p1-' + deposit, species, t, time, p2p1_total, axis)
+
+    return
+
 def deposit_triangles_dispersion(cartcomm, pos, mom, charge, grid, window, box, output_folder, species, t, time, axis):
     rank = cartcomm.Get_rank()
 
@@ -445,6 +486,12 @@ def save_triangle_fields_parallel_2d(comm, species, t, raw_folder,
     axis_lagrangian[1,0] = 0.0
     axis_lagrangian[1,1] = n_proc_x * n_ppp_x
 
+    axis_p2p1 = np.zeros_like(axis)
+    axis_p2p1[0,0] = -1.0
+    axis_p2p1[0,1] = 1.0
+    axis_p2p1[1,0] = -0.5
+    axis_p2p1[1,1] = 1.5
+
     if (rank==0):
         t_start = MPI.Wtime()
 
@@ -464,11 +511,16 @@ def save_triangle_fields_parallel_2d(comm, species, t, raw_folder,
     # Parameters for PSI
     grid = (deposit_n_x, deposit_n_y)
     grid_r = (deposit_n_x*4, deposit_n_y*4)
+    
     window = ((axis[0,0], axis[1,0]), (axis[0,1], axis[1,1]))
     box = window
+    
     window_lagrangian = ((axis_lagrangian[0,0], axis_lagrangian[1,0]), (axis_lagrangian[0,1], axis_lagrangian[1,1]))
     box_lagrangian = window_lagrangian
 
+    window_p2p1 = ((axis_p2p1[0,0], axis_p2p1[1,0]), (axis_p2p1[0,1], axis_p2p1[1,1]))
+    box_p2p1 = window_p2p1
+    
     particle_velocities = oi.momentum_to_velocity(particle_momentum)
 
     # First do particle deposits
@@ -490,6 +542,7 @@ def save_triangle_fields_parallel_2d(comm, species, t, raw_folder,
                           output_folder + sub_folder, species, t, time, axis, deposit='ngp')
     deposit_particles_psi(cartcomm, pos, mom, vel, charge_r, grid_r, window, box,
                           output_folder + sub_folder_r, species, t, time, axis, deposit='ngp')
+    deposit_particles_p2p1(cartcomm, mom, charge, grid, window_p2p1, box_p2p1, output_folder + sub_folder, species, t, time, axis_p2p1, deposit='ngp')
 
     if (rank==0):
         t_end = MPI.Wtime()
@@ -504,6 +557,7 @@ def save_triangle_fields_parallel_2d(comm, species, t, raw_folder,
                           output_folder + sub_folder, species, t, time, axis, deposit='cic')
     deposit_particles_psi(cartcomm, pos, mom, vel, charge_r, grid_r, window, box,
                           output_folder + sub_folder_r, species, t, time, axis, deposit='cic')
+    deposit_particles_p2p1(cartcomm, mom, charge, grid, window_p2p1, box_p2p1, output_folder + sub_folder, species, t, time, axis_p2p1, deposit='cic')
 
     if (rank==0):
         t_end = MPI.Wtime()
@@ -551,7 +605,7 @@ def save_triangle_fields_parallel_2d(comm, species, t, raw_folder,
                                           box_lagrangian, output_folder + sub_folder,
                                           species, t, time, axis_lagrangian)
     deposit_triangles_current_points(cartcomm, pos, vel, charge, grid, window, box,
-                                     output_folder + sub_folder, species, t, time, axis)
+                                     output_folder + sub_folder, species, t, time, axis)#
 
     deposit_triangles_current(cartcomm, pos, vel, charge_r, grid_r, window, box,
                               output_folder + sub_folder_r, species, t, time, axis)
@@ -563,6 +617,8 @@ def save_triangle_fields_parallel_2d(comm, species, t, raw_folder,
                                           species, t, time, axis_lagrangian)
     deposit_triangles_current_points(cartcomm, pos, vel, charge_r, grid_r, window, box,
                                      output_folder + sub_folder_r, species, t, time, axis)
+
+    deposit_triangles_p2p1(cartcomm, mom, charge, grid, window_p2p1, box_p2p1, output_folder + sub_folder, species, t, time, axis_p2p1)
 
 
     if (rank==0):